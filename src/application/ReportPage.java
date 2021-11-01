661aa1e48b23dc6043da2f636b5d76edf35bc0a7
Yuriliu0909 <liuziyu99@gmail.com>
2021-11-01 13:14:44 +1300

    add report page and class

--- src/application/ReportPage.java
+++ src/application/ReportPage.java
@@ -1,18 +1,87 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-public class ReportPage extends Application {
+public class ReportPage extends Application implements Initializable{
 	private static Stage stage;
+    @FXML
+    private LineChart<?, ?> lineChart;
+
+    @FXML
+    private CategoryAxis date;
+
+    @FXML
+    private NumberAxis revenue;
+
+    @FXML
+    private DatePicker calendar;
+
+    @FXML
+    private ToggleButton toggleGroup;
+
+    @FXML
+    private ToggleButton dayButton;
+
+    @FXML
+    private ToggleButton monthButton;
+
+    @FXML
+    private ToggleButton yearButton;
+    
+    //switch toggle button
+    @FXML
+    void ToggleButton(ActionEvent event) {
+    	if(event.getSource()==dayButton) {
+    		//add implementation
+    		
+    	}
+    	if(event.getSource()==monthButton) {
+    		//add implementation
+    		
+    	}
+    	if(event.getSource()==yearButton) {
+    		//add implementation
+    		
+    	}
+    }
+    
+
+
+//add data into line chart
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		XYChart.Series series= new XYChart.Series<>();
+		series.getData().add(new XYChart.Data("1",23));
+		series.getData().add(new XYChart.Data("3",13));
+		series.getData().add(new XYChart.Data("4",40));
+		series.getData().add(new XYChart.Data("5",67));
+		series.getData().add(new XYChart.Data("6",12));
+		series.getData().add(new XYChart.Data("7",10));
+		
+		lineChart.getData().addAll(series);
+	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
 		Parent root = FXMLLoader.load(ReportPage.class.getResource("/layout/ReportPageLayout.fxml"));
 		primaryStage.setScene(new Scene(root, 800, 600));
 		primaryStage.show();
@@ -50,4 +119,5 @@ public class ReportPage extends Application {
 			e1.printStackTrace();
 		}
     }
+
 }
