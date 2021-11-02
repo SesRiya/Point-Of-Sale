b2f6966bec71797a2b966cc2d553d65706f4b061
SesRiya <crdgorospe@gmail.com>
2021-11-03 11:11:37 +1300

    SalesPageTest

--- src/test/SalesPageTest.java
+++ src/test/SalesPageTest.java
@@ -2,10 +2,20 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import application.Coffee;
 import application.SalesPage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 class SalesPageTest {
 
@@ -16,6 +26,35 @@ class SalesPageTest {
 		salesPage = new SalesPage();
 	}
 	
+	@ParameterizedTest(name="{index}:totalPriceTest(0,1)")
+	@MethodSource("coffeProvider")
+	public void totalPriceTest(List<Coffee> coffees, double totalPrice) {
+		ObservableList<Coffee> coffeesOL = FXCollections.observableArrayList();	
+		for(Coffee coffee: coffees ) {
+			coffeesOL.add(coffee);
+		}				
+		assertEquals(totalPrice, salesPage.totalPrice(coffeesOL));
+	}
+	
+	public static Collection coffeProvider() {
+		return Arrays.asList(new Object[][] {{
+				Arrays.asList(
+						new Coffee(1, "Americano", "S", "Regular", "None", 5.50),
+						new Coffee(2, "Cappuccino", "S", "Regular", "None", 4.50),
+						new Coffee(4, "Espresso", "S", "Regular", "None", 4.00)
+						),
+				14
+				},
+			{
+					Arrays.asList(
+							new Coffee(1, "Americano", "S", "Regular", "None", 7.50),
+							new Coffee(2, "Cappuccino", "S", "Regular", "None", 5.50),
+							new Coffee(4, "Espresso", "S", "Regular", "None", 5.00)
+							),
+					18	
+			}
+		});
+	}
 	
 
 }
